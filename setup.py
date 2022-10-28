2ef48a0822d9cac5eb7a4ca66dce52c2c33e7727
SeanDisaro <116873610+SeanDisaro@users.noreply.github.com>
2022-10-28 17:48:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = "HelloWorldPackage"
   version= "0.0.1"
   description = "This is a description"
-  url = "https://github.com/SeanDisaro/TestRepo.git"
+  url = "git@github.com:SeanDisaro/TestRepo.git"
   author = "Sean Disaro"
   author_email = "seandisaro@gmail.com"
   license = "unlicense"
